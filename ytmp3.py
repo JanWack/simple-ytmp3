9233ccbb8c0693701546fb6803b749b79f824735
JanWack <boden.johan@gmail.com>
2026-02-16 23:22:48 +0100

    updated the gui. It looks SOOOOO much better.

--- ytmp3.py
+++ ytmp3.py
@@ -8,9 +8,9 @@ import sys
 
 """
 This is a simple porogram that converts YouTube videos to m4a.
-It uses ytl-dlp as its backend. It was created to make it easier
-for my friends (that doe sknow what a terminal is) to extraxt the
-audio from YouTube vides (for personal use only!).
+It uses ytl-dlp as its backend. It was originally created to make
+it easier for my friends (that doe sknow what a terminal is) to
+extraxt the audio from YouTube vides (for personal use only!).
 
 Written by: Johan Bodén, JanWack (GitHub)
 
@@ -42,56 +42,50 @@ def app():
     The main function. Not really good code-quality by having
     everything in one file hehe.
     """
-
-    #background_color = "#26242f"
-
     URLs = []
 
     root = tk.Tk()
     root.title("Easy ytmp3")
-    root.geometry('800x600')
-    #root.configure(bg=background_color)
+    root.geometry('600x400')
+    root.resizable(0, 0)
 
-    #label_style = ttk.Style()
-    #button_style = ttk.Style()
-    #label_style.configure("TLabel", background=background_color, foreground="#FFFFFF")
-    #button_style.configure("TButton", background=background_color, foreground="#FFFFFF")
-
-    url_var = tkinter.StringVar()
-    destination_path = tkinter.StringVar(value="")
+    url_var = tk.StringVar(root)
+    destination_path = tk.StringVar(root)
+    audio_format = tk.StringVar(root, "m4a") # Default is m4a
 
     def exit_prog():
         root.destroy()
         sys.exit(0)
 
-    root.grid_columnconfigure(0, weight=1)
-    root.grid_rowconfigure(0, weight=0)
-    root.grid_rowconfigure(1, weight=0)
-    root.grid_rowconfigure(2, weight=0)
-    root.grid_rowconfigure(3, weight=1)
-    root.grid_rowconfigure(4, weight=0)
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=0)
+    root.rowconfigure(1, weight=1)
+    root.rowconfigure(2, weight=0)
+    root.rowconfigure(3, weight=0)
+
+    frame0 = tk.Frame(root)
+    frame0.columnconfigure(0, weight=0)
+    frame0.columnconfigure(1, weight=1)
+    frame0.grid(row=0, column=0, sticky='ew')
 
     frame1 = tk.Frame(root)
-    frame1.grid(row=0, column=0, sticky='ew')
-    frame1.grid_columnconfigure(0, weight=0)
-    frame1.grid_columnconfigure(1, weight=1)
+    frame1.columnconfigure(0, weight=1)
+    frame1.columnconfigure(1, weight=0)
+    frame1.rowconfigure(0, weight=1)
+    frame1.grid(row=1, column=0, sticky='ewns')
+
     frame2 = tk.Frame(root)
-    frame2.grid(row=1, column=0, sticky='ew')
-    frame2.grid_columnconfigure(0, weight=0)
-    frame2.grid_columnconfigure(1, weight=1)
+    frame2.columnconfigure(0, weight=0)
+    frame2.columnconfigure(1, weight=0)
+    frame2.columnconfigure(2, weight=0)
+    frame2.columnconfigure(3, weight=1)
+    frame2.columnconfigure(4, weight=0)
+    frame2.grid(row=2, column=0, sticky='ew')
+
     frame3 = tk.Frame(root)
-    frame3.grid(row=2, column=0, sticky='ew')
-    frame3.grid_columnconfigure(0, weight=0)
-    frame3.grid_columnconfigure(1, weight=1)
-    frame3.grid_columnconfigure(2, weight=0)
-    frame4 = tk.Frame(root)
-    frame4.grid(row=3, column=0, sticky='ew')
-    frame4.grid_columnconfigure(0, weight=1)
-    frame4.grid_columnconfigure(1, weight=0)
-    frame5 = tk.Frame(root)
-    frame5.grid(row=4, column=0, sticky='ew')
-    frame5.grid_columnconfigure(0, weight=0)
-    frame5.grid_columnconfigure(1, weight=1)
+    frame3.columnconfigure(0, weight=0)
+    frame3.columnconfigure(1, weight=1)
+    frame3.grid(row=3, column=0, sticky='ew')
 
     def browse_files() -> bool:
         path = filedialog.askdirectory(initialdir=".", title="Select destination folder", mustexist=True)
@@ -108,24 +102,24 @@ def app():
             URLs.append(str_url)
             url_list.insert(url_list.size(), str_url)
 
-    add_item_text = ttk.Label(frame2, text="Add URL (video/playlist):", style='TLabel')
-    input = ttk.Entry(frame2, textvariable=url_var)
-    add_button = ttk.Button(frame2, text="Add", command=add_entry, style='TButton')
-    clear_button = ttk.Button(frame2, text="Clear", command= lambda: input.delete(0, tkinter.END), style='TButton')
-    add_item_text.grid(row=1, column=0, sticky='w', pady=10, padx=10)
-    input.grid(row=1, column=1, sticky='ew', padx=10, pady=10)
-    add_button.grid(row=1, column=2, sticky='e', padx=0, pady=10)
-    clear_button.grid(row=1, column=3, sticky='e', padx=10, pady=10)
+    add_item_text = ttk.Label(frame0, text="Add URL (video or playlist):")
+    input = ttk.Entry(frame0, textvariable=url_var)
+    add_button = ttk.Button(frame0, text="+", command=add_entry, width=2)
+    clear_button = ttk.Button(frame0, text="x", command= lambda: input.delete(0, tkinter.END), width=2)
+    add_item_text.grid(row=0, column=0, sticky='w', pady=10, padx=10)
+    input.grid(row=0, column=1, sticky='ew', padx=10, pady=10)
+    add_button.grid(row=0, column=2, sticky='e', padx=0, pady=10)
+    clear_button.grid(row=0, column=3, sticky='e', padx=10, pady=10)
 
     def begin_download():
         if not URLs:
             tkinter.messagebox.showinfo(title="No items", detail="There is nothing to download.", icon='info')
         else:
             if browse_files():
-                prog_label.configure(text="Downloading")
-                progress.grid(row=4, column=1, sticky='w', padx=10, pady=10)
+                prog_label.configure(text="Downloading, please wait.")
+                progress.grid(row=0, column=1, sticky='ew')
                 progress.start(4)
-                thread1 = threading.Thread(target=download_items, args=(URLs, destination_path.get(), progress, prog_label))
+                thread1 = threading.Thread(target=download_items, args=(URLs, destination_path.get(), progress, prog_label, audio_format.get()))
                 thread1.start()
             else:
                 return
@@ -134,41 +128,52 @@ def app():
         url_list.delete(0, url_list.size())
         URLs.clear()
 
-    url_list = tk.Listbox(frame4, bg="grey", selectmode='single', height=20)
-    download = ttk.Button(frame3, text="Download listed items", command=begin_download, style='TButton')
-    bindings = ttk.Label(frame3, text="Enter: add item\tBackspace: remove item", style='TLabel')
-    clear = ttk.Button(frame3, text="Clear list", command=clear_list, style='TButton')
-    download.grid(row=2, column=0, sticky='w', padx=10, pady=10)
-    bindings.grid(row=2, column=1, sticky='e', padx=10, pady=10)
-    clear.grid(row=2, column=2, sticky="e", padx=10, pady=10)
-    url_list.grid(row=3, column=0, sticky='nsew', padx=10, pady=10)
-
-    scroll = ttk.Scrollbar(frame4, orient=tkinter.VERTICAL, command=url_list.yview)
-    url_list['yscrollcommand'] = scroll.set
-    scroll.grid(row=3, column=1, sticky='ns', padx=10, pady=10)
-
-    prog_label = ttk.Label(frame5, text="No jobs", style='TLabel')
-    progress = ttk.Progressbar(frame5, orient=tk.HORIZONTAL, mode='indeterminate', length=400)
-    prog_label.grid(row=4, column=0, sticky='w', padx=10, pady=10)
-
-
-    def add_entry_button(event):
-        add_entry()
-    
-    def remove_entry(event):
+    def remove_entry():
         selected_indx = url_list.curselection()
         if selected_indx:
             selected = url_list.selection_get()
             url_list.delete(selected_indx)
             URLs.remove(selected)
 
+    url_list = tk.Listbox(frame1, bg="grey", selectmode='single')
+    scroll = ttk.Scrollbar(frame1, orient=tkinter.VERTICAL, command=url_list.yview)
+    url_list['yscrollcommand'] = scroll.set
+    scroll.grid(row=0, column=1, sticky='nse', padx=10)
+    url_list.grid(row=0, column=0, sticky='nsew', padx=10)
+
+    download = ttk.Button(frame2, text="Download", command=begin_download)
+    mp3_button = ttk.Radiobutton(frame2, text="mp3", variable=audio_format, value="mp3")
+    m4a_button = ttk.Radiobutton(frame2, text="m4a", variable=audio_format, value="m4a")
+
+    audio_format.set("m4a")
+
+    remove_entry_button = ttk.Button(frame2, text="Remove item", command=remove_entry)
+    clear = ttk.Button(frame2, text="Clear list", command=clear_list)
+    download.grid(row=0, column=0, sticky='w', padx=10, pady=10)
+    m4a_button.grid(row=0, column=1, sticky='w')
+    mp3_button.grid(row=0, column=2, sticky='w', padx=10)
+    remove_entry_button.grid(row=0, column=3, sticky='e')
+    clear.grid(row=0, column=4, sticky="e", padx=10, pady=10)
+
+
+    prog_label = ttk.Label(frame3, text="No jobs")
+    progress = ttk.Progressbar(frame3, orient=tk.HORIZONTAL, mode='indeterminate')
+    prog_label.grid(row=0, column=0, sticky='w', padx=10, pady=5)
+
+
+    def add_entry_button(event):
+        add_entry()
+    
+    def remove_entry_button(event):
+        remove_entry()
+
     input.bind("<Return>", add_entry_button)
-    url_list.bind("<BackSpace>", remove_entry)
+    url_list.bind("<BackSpace>", remove_entry_button)
     root.protocol("WM_DELETE_WINDOW", exit_prog)
     root.mainloop()
 
 
-def download_items(uris: list, path: str, prog: ttk.Progressbar, lab: tk.Label):
+def download_items(uris: list, path: str, prog: ttk.Progressbar, lab: tk.Label, audio_format: str):
     """
     Based on the exmaple from yt-dlp
     
@@ -182,10 +187,10 @@ def download_items(uris: list, path: str, prog: ttk.Progressbar, lab: tk.Label):
     :type lab: Label
     """
     ydl_opts = {
-        'format': 'm4a/bestaudio/best',
+        'format': audio_format + '/bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'm4a',
+            'preferredcodec': audio_format,
         }],
         'paths' : {
             'home' : path
